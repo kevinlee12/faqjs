a35a4bef340920d47d19d311e387ecdd7594d752
Kevin Lee <leekevin@berkeley.edu>
2018-01-10 23:04:01 -0800

    Add active attribute to navbar in admin

--- public/javascripts/pages/admin/navbar.js
+++ public/javascripts/pages/admin/navbar.js
@@ -1,20 +1,31 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
+const links = [
+  { url: '/admin/manage/threads', name: 'Thread'},
+  { url: '/admin/manage/users', name: 'Users' },
+  { url: '/admin/manage/elastic', name: 'Elastic Search' }
+]
+
 class Navbar extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    var navLinks = links.map(function(item) {
+      return (
+        <li class={ window.location.pathname === item.url ? 'active' : null }>
+          <a href={ item.url }> { item.name } </a>
+        </li>
+      );
+    });
     return (
       <div class="navbar-fixed">
         <nav class="nav-extended">
           <div class="nav-wrapper">
             <div class="brand-logo left">FAQ</div>
             <ul class="right">
-              <li><a href="/admin/manage/users">Users</a></li>
-              <li><a href="/admin/manage/threads">Thread</a></li>
-              <li><a href="/admin/manage/elastic">Elastic Search</a></li>
+              { navLinks }
               <li><a href="/logout" class="waves-effect waves-light btn blue-grey darken-2" >Logout</a></li>
             </ul>
           </div>
